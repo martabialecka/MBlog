babcd56cd8007e0b510462e298251e98c9ab1401
martabialecka <marta.bialecka@outlook.com>
2023-03-29 00:31:34 +0200

    Refactoring

--- blog/routes.py
+++ blog/routes.py
@@ -8,34 +8,43 @@ def index():
     all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     return render_template('homepage.html', all_posts=all_posts)
 
-@app.route('/new-post/', methods=['GET', 'POST'])
-def create_entry():
-    form = EntryForm()
+def entry(entry_id):
+    '''
+    entry_id == -1 means a new entry
+    entry_id > -1 means edit the entry at entry_id
+    '''
+    if entry_id != -1:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        flash_message = 'Wpis zmieniony!'
+    else:
+        entry = None
+        form = EntryForm()
+        flash_message = 'Dodano nowy wpis!'
     errors = None
+
     if request.method == 'POST':
         if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
+            if entry is not None:
+                form.populate_obj(entry)
+            else:
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
             db.session.commit()
-            flash('Dodano nowy wpis!')
+            flash(flash_message)
         else:
             errors = form.errors
+
     return render_template('entry_form.html', form=form, errors=errors)
 
-@app.route('/edit-post/<int:entry_id>', methods=['GET', 'POST'])
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return entry(-1)
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            flash('Wpis zmieniony!')
-        else:
-            errors = form.errors
-    return render_template('entry_form.html', form=form, errors=errors)
+    return entry(entry_id)
